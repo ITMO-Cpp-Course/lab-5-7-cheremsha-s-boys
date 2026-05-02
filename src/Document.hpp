9cf559a5f4fa5efa37e9798d26d98f45c7e2adb7
lab1 <GenyaSH2007@yandex.ru>
2026-05-02 12:03:21 +0300

    #1

---
+++ src/Document.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <utility>
+namespace lab5::memory
+{
+struct Document
+{
+
+    size_t id;
+    std::string name;
+    std::string content;
+
+    Document(size_t id, std::string name, std::string content)
+        : id(id), name(std::move(name)), content(std::move(content)) {};
+    ~Document() = default;
+    Document(const Document&) = delete;
+    Document& operator=(const Document&) = delete;
+    Document(Document&& other) noexcept = default;
+    Document& operator=(Document&& other) noexcept = default;
+};
+} // namespace lab5::memory
